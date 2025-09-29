c79562b6b5dbc92e827aab8ad81305efe287b5f0
earthtraveller1 <liangyuezhao7@gmail.com>
2025-09-29 17:10:33 -0400

    Bootstrapped a simple React application using the following. - Node.js - Webpack. - Babel. - React.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as ReactDOM from "react-dom/client"
+import * as React from "react"
+
+function Hello() {
+    return <p>Hello!</p>
+}
+
+ReactDOM.createRoot(document.getElementById("app")).render(<Hello />)
+
