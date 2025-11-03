1eed9998181c7767b4318912400cb82372e8db74
earthtraveller1 <liangyuezhao7@gmail.com>
2025-11-03 13:55:17 -0500

    Added some endpoints that adds some dummy data.

---
+++ src/common.js
@@ -0,0 +1,23 @@
+export class CardStack {
+    /**
+     * @type string
+     * @public
+     */
+    name
+
+    /**
+     * @type number
+     * @public
+     */
+    id
+
+    /**
+    * @param {string} name
+    * @param {number} id 
+    */
+    constructor(name, id) {
+        this.name = name
+        this.id = id
+    }
+}
+
--- src/main.js
+++ src/main.js
@@ -1,16 +1,27 @@
 import express from "express"
+import { CardStack } from "./common.js"
 
 function main() {
     const app = express()
     let port = 6969
     if (process.env.PORT != undefined && Number.isInteger(process.env.PORT)) {
-        port = process.env.PORT
+        port = parseInt(process.env.PORT)
     }
 
     app.use(express.static("dist"))
 
     app.get("/", (_req, res) => {
         res.sendFile(`${process.cwd()}/index.html`)
+    })
+
+    app.get("/api/stacks", (_req, res) => {
+        // For now, we do not have a database, so we are just going to return
+        // some dummy values.
+        res.json([
+            new CardStack("Chemistry", 0),
+            new CardStack("Biology", 1),
+            new CardStack("Yeezy", 2)
+        ])
     })
 
     app.listen(port, () => {
