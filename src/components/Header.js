d42613c3d4d2e92fd12ee4b52f07c5a7779c76c8
earthtraveller1 <liangyuezhao7@gmail.com>
2025-11-03 13:34:42 -0500

    Added a basic header for the page.

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return ( 
+        <div className="flex justify-start bg-slate-900"> 
+            <h1 className="text-xl p-4 font-bold text-slate-100">
+                Neng's Flashcards
+            </h1>
+        </div> 
+    )
+}
