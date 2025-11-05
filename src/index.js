0a3965c11b1e2f08058cc98b0153c55ea49d9582
earthtraveller1 <liangyuezhao7@gmail.com>
2025-11-05 10:59:41 -0500

    Added a small title bar to the stack list.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import CardStacks from "./components/CardStacks.js"
 function Index() {
     return <div className="text-slate-100">
         <Header />
+        <h1 className="text-lg text-slate-400 font-bold px-4 pt-4">Your Stacks</h1>
         <CardStacks />
     </div>
 }
