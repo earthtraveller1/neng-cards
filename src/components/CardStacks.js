837596f2dcc42de4792472aa02464b6430b727dc
earthtraveller1 <liangyuezhao7@gmail.com>
2025-11-30 15:38:50 -0500

    Added the dialog box that is needed for one to add new stacks.

--- src/components/CardStacks.js
+++ src/components/CardStacks.js
@@ -1,8 +1,13 @@
 import React from "react"
-import { CardStack } from "../common.js"
+import { CardStack, NewCardStack } from "../common.js"
+import Dialog from "./Dialog.js"
+import InputField from "./InputField.js"
+import Button from "./Button.js"
 
 export default function CardStacks() {
     const [stacks, setStacks] = React.useState(/** @type {CardStack[]} */([]))
+    const [newStackDialog, setNewStackDialog] = React.useState(false)
+    const [newStackName, setNewStackName] = React.useState("")
 
     React.useEffect(() => {
         (async () => {
@@ -14,19 +19,43 @@ export default function CardStacks() {
         })().then(stacks => {
             setStacks(stacks)
         })
-    }, [])
+    }, [newStackDialog])
 
     return (
-        <div className="flex flex-row flex-wrap">
-            {stacks.map((stack) => {
-                return <button className={ 
-                    "m-4 flex min-h-64 w-sm flex-col-reverse p-4 bg-slate-800 " + 
+        <>
+            <div className="flex flex-row items-start">
+                <h1 className="text-xl text-slate-400 font-bold px-4 pt-4">Your Stacks</h1>
+                <Button onClick={() => {
+                    setNewStackDialog(true)
+                }}>New Stack</Button>
+            </div>
+            <div className="flex flex-row flex-wrap">
+                {stacks.map((stack) => {
+                    return <button className={
+                        "m-4 flex min-h-64 w-sm flex-col-reverse p-4 bg-slate-800 " +
                         "border-solid border-4 rounded-xl select-none " +
                         "hover:bg-slate-300 hover:text-slate-800 hover:border-8 duration-300"
-                }>
-                    <div className="text-center font-bold">{stack.name}</div>
-                </button>
-            })}
-        </div>
+                    }>
+                        <div className="text-center font-bold">{stack.name}</div>
+                    </button>
+                })}
+            </div>
+
+            {newStackDialog && <Dialog>
+                <h1 className="text-lg">Create a new Stack</h1>
+                <InputField name="Name" onInput={setNewStackName} />
+                <Button onClick={() => {
+                    fetch("/api/stacks", {
+                        method: "POST",
+                        body: JSON.stringify(new NewCardStack(newStackName)),
+                        headers: {
+                            "Content-Type": "application/json"
+                        }
+                    }).then(() => {
+                        setNewStackDialog(false)
+                    })
+                }}>Okay</Button>
+            </Dialog>}
+        </>
     )
 }
