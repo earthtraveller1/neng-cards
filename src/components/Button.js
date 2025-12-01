22f53b2263bc180ebce5813c1c1d8a9ddec235a8
earthtraveller1 <liangyuezhao7@gmail.com>
2025-12-01 16:47:06 -0500

    Added a way for the client to specify custom styles.

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,20 +3,27 @@ import React from "react"
 class ButtonProps {
     /** @type {React.MouseEventHandler} */
     onClick
+
+    /** @type {string?} */
+    className
 }
 
 /**
  * @param {React.PropsWithChildren<ButtonProps>} props
  */
 export default function Button(props) {
+    let styles = "px-3 py-1 m-2 border-solid rounded-xl bg-cyan-400 border-cyan-400 " +
+        "hover:border-cyan-300 hover:bg-cyan-300 text-black hover:rounded-lg " +
+        "active:border-green-300 active:bg-green-300 duration-250 active:rounded-2xl"
+
+    if (props.className != null) {
+        styles += " " + props.className
+    }
+
     return (
         <button 
             onClick={props.onClick}
-            className={
-                "px-3 py-1 m-2 border-solid rounded-xl bg-cyan-400 border-cyan-400 " +
-                    "hover:border-cyan-300 hover:bg-cyan-300 text-black hover:rounded-lg " +
-                    "active:border-green-300 active:bg-green-300 duration-250 active:rounded-2xl"
-            }
+            className={styles}
         >
             {props.children}
         </button>
--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,13 +1,19 @@
 import React from "react"
 
-/** @param {{ name: string, onInput: (value: string) => void}} props */
+/** @param {{ name: string, onInput: (value: string) => void, className: string?}} props */
 export default function InputField(props) {
+    let style = "border-solid border-b-4 border-zinc-700 hover:border-cyan-300 " +
+        "focus:border-blue-500 duration-250 outline-none"
+
+    if (props.className != null) {
+        style += " " + props.className
+    }
+
     return (
         <div className="flex flex-row">
             <p className="pr-2">{props.name}</p>
             <input
-                className={"border-solid border-b-4 border-zinc-700 hover:border-cyan-300 " +
-                    "focus:border-blue-500 duration-250 outline-none"}
+                className={style}
                 onInput={input => {
                     props.onInput(input.currentTarget.value)
                 }}
