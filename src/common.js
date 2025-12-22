f92d1c14ad5e515b0448480380c5a15b0cdb0227
earthtraveller1 <liangyuezhao7@gmail.com>
2025-12-22 12:56:21 -0500

    Added the ability to easily change the color of buttons.

--- src/common.js
+++ src/common.js
@@ -11,3 +11,12 @@ import { ObjectId } from "mongodb"
  * @property {string} frontText
  * @property {string} backText
  */
+
+/**
+ * @enum {string}
+ */
+export const Colors = {
+    CYAN: "bg-cyan-400 hover:bg-cyan-300 active:bg-green-300 text-black",
+    GREEN: "bg-green-400 hover:bg-green-300 active:bg-cyan-300 text-black",
+    RED: "bg-red-700 hover:bg-red-600 active:bg-red-900 text-white"
+}
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,19 @@
 import React from "react"
+import { Colors } from "../common.js"
 
 /**
  * @typedef {Object} ButtonProps
  * @property {React.MouseEventHandler} onClick
  * @property {string} [className]
+ * @property {Colors} color
  */
 
 /**
  * @param {React.PropsWithChildren<ButtonProps>} props
  */
 export default function Button(props) {
-    let styles = "px-3 py-1 border-solid rounded-xl bg-cyan-400 border-cyan-400 " +
-        "hover:border-cyan-300 hover:bg-cyan-300 text-black hover:rounded-lg " +
+    let styles = "px-3 py-1 border-solid rounded-xl " +
+        `${props.color} hover:rounded-lg ` +
         "active:border-green-300 active:bg-green-300 duration-250 active:rounded-2xl"
 
     if (props.className != null) {
